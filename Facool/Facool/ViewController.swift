46da45fa886d5c680ad82104f5a10d1e4debd7b6
Alexandre do Vale <vale.apv@gmail.com>
2017-11-28 00:10:17 -0200

    Facool
    Criação do Storyboard e layout básico

---
+++ Facool/Facool/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Facool
+//
+//  Created by Student on 24/11/17.
+//  Copyright © 2017 Alexandre do Vale, Felipe Arakaki, Luís Gustavo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
